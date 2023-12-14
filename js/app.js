7a43b2a7d0d9723a005d78f6e73e891b91d0e44f
Chori <yidodeveloper@gmail.com>
2023-12-14 20:37:35 +0900

    그리기 기능 완성

--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
 let lineWidth = document.querySelector("#line-width");
+const color = document.querySelector("#color");
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
+const modeBtn = document.querySelector("#mode-btn");
+const destroyBtn = document.querySelector("#destroy-btn");
+const eraserBtn = document.querySelector("#eraser-btn");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 600;
-canvas.height = 600;
+
+const CANVAS_WIDTH = 600;
+const CANVAS_HEIGHT = 600;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
+let isFilling = false;
 
 const colors = [
   "#ff3838",
@@ -28,11 +40,11 @@ function draw(event) {
   ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-function startPainting(event) {
+function startPainting() {
   isPainting = true;
 }
 
-function cancelPainting(event) {
+function cancelPainting() {
   isPainting = false;
   ctx.beginPath();
 }
@@ -41,9 +53,55 @@ function onLineWidthChange(event) {
   ctx.lineWidth = event.target.value;
 }
 
+function onChangeColor(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+}
+
+function onModeClick() {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
+}
+
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+}
+
+function onDestroyClick() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserClick() {
+  ctx.strokeStyle = "white";
+  isFilling = false;
+  modeBtn.innerText = "Fill";
+}
+
 canvas.addEventListener("mousemove", draw);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("moseleave", cancelPainting);
+canvas.addEventListener("mousedown", onCanvasClick);
 
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onChangeColor);
+
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
